0e06aee35d801ee66781c2576a26b512204ed01f
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2021-08-27 13:36:04 +0300

    Show message for empty folders

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -5,12 +5,15 @@
     <table class="grid">
       <Row v-for="entry of listLimited" :entry="entry"/>
     </table>
+    <div class="empty" v-if="empty">
+      <span>The folder is empty.</span>
+    </div>
   </div>
 </template>
 
 <script setup>
 import Row from "./Row.vue";
-import {back, listLimited} from "../store.js";
+import {back, empty, listLimited} from "../store.js";
 
 function onContextMenu(event) {
   event.preventDefault();
@@ -22,5 +25,13 @@ function onContextMenu(event) {
 .content, table {
   width: 100%;
   overflow: auto;
+}
+.empty {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: var(--gray-2);
 }
 </style>
