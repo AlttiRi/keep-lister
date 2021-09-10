67eda8f5f0c4908cbd6a421e814ab57bd5950d43
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2021-09-10 18:32:40 +0300

    Add iterate data util functions

--- src/util.js
+++ src/util.js
@@ -90,3 +90,60 @@ export function appendScript(src, integrity) {
         document.querySelector("body").append(script);
     });
 }
+
+
+export async function *iterateReadableStream(stream) {
+    const reader = stream.getReader();
+    while (true) {
+        const {done, /** @type {Uint8Array} */ value} = await reader.read();
+        if (done) {
+            break;
+        }
+        yield value;
+    }
+}
+
+// chunkSize is 65536, ReadableStream uses the same size.
+// There is no speed difference between using of different the chunk's sizes.
+export function *iterateArrayBuffer(arrayBuffer, chunkSize = 65536) {
+    const buffer = new Uint8Array(arrayBuffer);
+    let index = 0;
+    while (true) {
+        const chunk = buffer.subarray(index, index + chunkSize);
+        if (!chunk.length) {
+            break;
+        }
+        yield chunk;
+        index += chunkSize;
+    }
+}
+
+// Iterate Blob (or File)
+// Note: `chunkSize` affects the execution speed
+// It works with the same speed in Chromium, but a bit faster in Firefox (in comparison with `iterateBlob_v1`)
+export function *iterateBlob(blob, chunkSize = 2 * 1024 * 1024) {
+    let index = 0;
+    while (true) {
+        const blobChunk = blob.slice(index, index + chunkSize);
+        if (!blobChunk.size) {break;}
+
+        yield read(blobChunk);
+        index += chunkSize;
+    }
+
+    async function read(blob) {
+        return new Uint8Array(await blob.arrayBuffer());
+    }
+}
+
+export function bytesToSize(bytes, decimals = 2) {
+    if (bytes === 0) {
+        return "0 B";
+    }
+    const k = 1024;
+    decimals = decimals < 0 ? 0 : decimals;
+    const sizes = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+    return Number.parseFloat((bytes / Math.pow(k, i)).toFixed(decimals)) + " " + sizes[i];
+}
