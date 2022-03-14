422043e5c23304acf1c53a93b6fe37dda7e65b21
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2022-03-14 10:29:01 +0300

    Show a warning on mult `setScan` call

--- src/core/folders.js
+++ src/core/folders.js
@@ -21,6 +21,9 @@ export const parsingStateNumber = ref(0);
  * @return {Promise<void>}
  */
 export async function setScan(input) {
+    if (scanParsing.value) {
+        console.warn("[setScan][warning]: Multiple call"); // todo break `parseScan`
+    }
     scanParsing.value = true;
 
     let metaInited = false;
