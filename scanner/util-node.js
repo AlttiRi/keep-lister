db83cecb3dba6c094f525fe03e33ff3cf50a6227
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2022-04-04 18:25:29 +0300

    Add `eraseCursorLine`

--- scanner/util-node.js
+++ scanner/util-node.js
@@ -244,4 +244,5 @@ export const ANSI_RED_BOLD = text => "\u001B[1;31m" + text + _ANSI_RESET;
 
 export const saveCursorPosition    = () => process.stdout.write("\u001B[s");
 export const restoreCursorPosition = () => process.stdout.write("\u001B[u");
+export const eraseCursorLine       = () => process.stdout.write("\u001B[K");
 export const moveCursorToTop = (num = 1) => process.stdout.write(`\u001B[${num}A`);
